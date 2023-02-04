614af0af7c1fc48a17a4e874d51d460f64a93865
Almatik <almatiktk@gmail.com>
2023-02-04 21:04:53 +0600

    Update c3910000201.lua

--- script/c3910000201.lua
+++ script/c3910000201.lua
@@ -12,15 +12,16 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x8e}
-function s.costfilter(c,lp)
-	return c:IsSetCard(0x8e) and c:IsAttackBelow(lp)
+function s.costfilter(c,e,tp,lp)
+	return c:IsSetCard(0x8e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and c:IsAttackBelow(lp)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local lp=Duel.GetLP(tp)
 	local t={}
 	if chk==0 then return Duel.CheckLPCost(tp,100)
-		and Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,lp) end
-	local g=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_HAND,0,nil,lp)
+		and Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,e,tp,lp) end
+	local g=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_HAND,0,nil,e,tp,lp)
 	local tc=g:GetFirst()
 	for tc in aux.Next(g) do
 		table.insert(t,tc:GetAttack())
@@ -28,4 +29,23 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local cost=Duel.AnnounceNumber(tp,table.unpack(t))
 	Duel.PayLPCost(tp,cost)
 	e:SetLabel(cost)
+end
+function s.spfilter(c,e,tp,val)
+	return c:IsSetCard(0x8e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and c:GetAttack()==val
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local val=e:GetLabel()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp,val) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local val=e:GetLabel()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,val)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
