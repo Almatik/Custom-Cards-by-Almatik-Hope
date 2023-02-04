77aef1dee7f2396655e79a33b4ecef69a6214e98
Almatik <almatiktk@gmail.com>
2023-02-04 21:12:48 +0600

    Update c3910000201.lua

--- script/c3910000201.lua
+++ script/c3910000201.lua
@@ -29,18 +29,16 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 		table.insert(t,tc:GetAttack())
 	end
 	local cost=Duel.AnnounceNumber(tp,table.unpack(t))
-	Duel.PayLPCost(tp,cost)
-	local gc=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp,cost)
-	e:SetLabelObject(gc)
+	e:SetLabel(cost)
 end
 function s.spfilter(c,e,tp,val)
 	return c:IsSetCard(0x8e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and c:GetAttack()==val
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local val=e:GetLabel()
+	local lp=Duel.GetLP(tp)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp,val) end
+		and Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,e,tp,lp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
