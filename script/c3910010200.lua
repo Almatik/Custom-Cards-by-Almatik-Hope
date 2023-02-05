ca75b5f9606014e2b77e5c20524c11a904488b22
Almatik <almatiktk@gmail.com>
2023-02-05 19:47:03 +0600

    Update c3910010200.lua

--- script/c3910010200.lua
+++ script/c3910010200.lua
@@ -139,7 +139,7 @@ function s.Choose2Random3(e,tp)
 
 
 	local deckid1,deck2=Duel.SelectCardsFromCodes(tp,2,2,false,false,table.unpack(decklist))
-	local tc=Duel.CreateToken(tp,deckid1)
+	local tc=Duel.CreateToken(tp,deckid2)
 	Duel.SendtoHand(tc,tp,REASON_RULE)
 end
 
