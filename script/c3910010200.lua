6174a53eed5e2ed750aaa17855585a624c031f23
Almatik <almatiktk@gmail.com>
2023-02-05 18:51:46 +0600

    Update c3910010200.lua

--- script/c3910010200.lua
+++ script/c3910010200.lua
@@ -48,7 +48,6 @@ function s.DeleteDeck(tp)
 end
 function s.PlaceDeck(tp,deckid)
 	Duel.Hint(HINT_SKILL_FLIP,tp,deckid|(1<<32))
-	Duel.Hint(HINT_CARD,tp,deckid)
 end
 function s.ChooseDeck(e,tp)
 	--Collect All Decks/Packs
